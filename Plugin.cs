4f03ae08c0eb818003fd9de944359c4ff7405b40
ZedDevStuff <98429642+ZedDevStuff@users.noreply.github.com>
2022-08-28 14:26:17 +0000

    Created Fresh

---
+++ Plugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using ULTRAKILL;
+
+namespace Fresh
+{
+    [BepInPlugin("zed.fresh", "Fresh", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Difficulty difficulty;
+        ConfigEntry<Difficulty> cfg;
+        Harmony harm;
+
+        private void Awake()
+        {
+            Logger.LogInfo("Fresh");
+            cfg = Config.Bind<Difficulty>("Difficulty","Difficulty",Difficulty.Easy,"Difficulty of the mod");
+            difficulty = cfg.Value;
+            harm = Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+        public static void Kill(bool isDead)
+        {
+            if(!isDead)
+            {
+                MonoSingleton<NewMovement>.Instance.GetHurt(200,false,1,true);
+            }
+        }
+        
+        [HarmonyPatch(typeof(StyleHUD),"UpdateFreshnessSlider")]
+        [HarmonyPostfix]
+        static void Patch(StyleHUD __instance)
+        {
+            Plugin p = new Plugin();
+            NewMovement nm = MonoSingleton<NewMovement>.Instance;
+            string s = __instance.freshnessSliderText.text;
+            if(difficulty == Difficulty.Easy)
+            {
+                if(s.Contains("DULL"))
+                {
+                    Kill(nm.dead);
+                }
+            }
+            else if(difficulty == Difficulty.Normal)
+            {
+                if(s.Contains("STALE"))
+                {
+                    Kill(nm.dead);
+                }
+            }
+            else if(difficulty == Difficulty.Hard)
+            {
+                if(s.Contains("USED"))
+                {
+                    Kill(nm.dead);
+                }
+            }
+        }
+    }
+}
+public enum Difficulty
+{
+    Disabled,
+    Easy,
+    Normal,
+    Hard
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Fresh
+Fresh is an Ultrakill mod that will kill you everytime your weapon freshness becomes dull. The default difficulty (Easy) kills you at dull, Medium at stale and Hard at used. To change the difficulty, open the zed.fresh.cfg located in your BepInEx/Config folder.
+
+# Installation
+This is a BepInEx mod. To install, just unzip the downloaded file inside BepInEx/plugin folder (or UKMM Mods if using UKMM)
